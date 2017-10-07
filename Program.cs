184498b0d3d4420c3db12da8267f1b2b53911f9e
Riza Marhaban <riza.marhaban@hotmail.com>
2017-10-07 14:26:38 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace pscoretest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                var results = ps.AddScript("Get-Command -CommandType cmdlet New-*").Invoke();
+
+                foreach (var item in results)
+                {
+                    Console.WriteLine(item.Properties["Name"].Value.ToString());
+                }
+            }
+        }
+    }
+}
